9c92eb79a48e2836f7de6d17ba4301e83ff6d539
Onise <tsotskhalashvili558@gmail.com>
2026-02-05 03:32:44 +0400

    Update EmailSender.cs

--- TechMarketplace.API/TechMarketplace.API/SMTP/EmailSender.cs
+++ TechMarketplace.API/TechMarketplace.API/SMTP/EmailSender.cs
@@ -7,11 +7,14 @@ namespace TechMarketplace.API.SMTP
     {
         public void SendMail(string to, string subject, string body)
         {
+            // ეს ხაზი აუცილებელია Railway/Linux-ისთვის
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
             SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
             smtpClient.EnableSsl = true;
             smtpClient.UseDefaultCredentials = false;
-
             smtpClient.Credentials = new NetworkCredential("tsotskhalashvili558@gmail.com", "wfit myhi rnml nwmz");
+
             MailMessage message = new MailMessage();
             message.From = new MailAddress("tsotskhalashvili558@gmail.com");
             message.To.Add(to);
@@ -20,8 +23,7 @@ namespace TechMarketplace.API.SMTP
             message.IsBodyHtml = true;
 
             smtpClient.Send(message);
-
-
         }
     }
+    
 }
