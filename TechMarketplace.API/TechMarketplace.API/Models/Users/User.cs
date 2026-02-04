702f101a59a966fdf9c4a3f02336515aa51cce0d
Onise <tsotskhalashvili558@gmail.com>
2026-02-05 01:45:45 +0400

    Dockerfile

--- TechMarketplace.API/TechMarketplace.API/Models/Users/User.cs
+++ TechMarketplace.API/TechMarketplace.API/Models/Users/User.cs
@@ -9,7 +9,8 @@ namespace TechMarketplace.API.Models.Users
 {
     public class User
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }  
+
 
         public string FirstName { get; set; }
 
