414d58962ea54ff80bd3b99d7022f6d61a6d544c
Onise <tsotskhalashvili558@gmail.com>
2026-02-05 02:53:27 +0400

    Update

--- TechMarketplace.API/TechMarketplace.API/Program.cs
+++ TechMarketplace.API/TechMarketplace.API/Program.cs
@@ -236,4 +236,6 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-app.Run();
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Run($"http://0.0.0.0:{port}");
